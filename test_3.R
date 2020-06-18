7ee6e95a203afb3391a9dd15b48e63bdc0138e60
solavrov <2slavrov@gmail.com>
2020-06-18 17:05:14 +0300

    test_3

--- test_3.R
+++ test_3.R
@@ -3,10 +3,12 @@ options("max.print"=50)
 sber <- load_stock_data_by_ticker('sber')
 gazp <- load_stock_data_by_ticker('gazp')
 lkoh <- load_stock_data_by_ticker('lkoh')
+chmf <- load_stock_data_by_ticker('chmf')
+gmkn <- load_stock_data_by_ticker('gmkn')
 
-l <- make_los(sber, gazp, lkoh)
+l <- make_los(sber, gazp, lkoh, chmf, gmkn)
 
-w <- c(0.4, 0.3, 0.3)
+w <- c(0.2, 0.2, 0.2, 0.2, 0.2)
 
 d <- l[[1]]$Date[2000]
 
@@ -17,3 +19,6 @@ d <- l[[1]]$Date[2000]
 
 sd_simple <- calc_sd_port_simple(l, w, calc_win = 250)
 sd_gogarch <- gogarch.calc_sd_port(l, w, calc_win = 1000)
+
+print_var_analytics(sd_simple)
+print_var_analytics(sd_gogarch)
